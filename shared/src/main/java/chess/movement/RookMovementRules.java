73d61c037083eee5d0e5e13bd632ea064b5f6f17
Wiley Welch <wileyjwelch1@gmail.com>
2024-01-20 18:30:26 -0700

    fixing the rook movement

--- shared/src/main/java/chess/movement/RookMovementRules.java
+++ shared/src/main/java/chess/movement/RookMovementRules.java
@@ -1,2 +1,20 @@
-package chess.movement;public class RookMovementRules {
+package chess.movement;
+
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+
+public class RookMovementRules extends ChessMovementRule{
+
+
+  @Override
+  public void chessMove(ChessBoard chessBoard, ChessPosition chessPosition, Collection<ChessMove> validMoves) {
+    preditions(chessBoard,chessPosition,1,0,validMoves);
+    preditions(chessBoard,chessPosition,0,1,validMoves);
+    preditions(chessBoard,chessPosition,-1,0,validMoves);
+    preditions(chessBoard,chessPosition,0,-1,validMoves);
+  }
+
 }
