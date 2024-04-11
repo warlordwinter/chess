4b725eaa278fc68da8ce0bd873fa5b5b4bb4e021
Wiley Welch <wileyjwelch1@gmail.com>
2024-04-11 08:37:42 -0600

    Finished ConnectionManager

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,30 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConnectionManager {
+  public final Map<Integer, Map<String, Session>> sessions = new HashMap<>();
+
+  void addSessionToGame(Integer gameID,String authToken, Session session){
+    if (!sessions.containsKey(gameID)){
+      sessions.put(gameID, new HashMap<>());
+    }
+    Map<String,Session> gameSessions =sessions.get(gameID);
+    gameSessions.put(authToken,session);
+  }
+
+  void removeSessionFromGame(Integer gameID,String authToken, Session session){
+
+    if(!sessions.containsKey(gameID)){
+      Map<String, Session> gameSessions = sessions.get(gameID);
+      gameSessions.remove(authToken);
+    }else{
+      System.out.println("GameID doesn't exist");
+    }
+  }
+
+  Map<String,Session> getSessionsFOrGame(Integer gameID){return sessions.get(gameID);}
 }
